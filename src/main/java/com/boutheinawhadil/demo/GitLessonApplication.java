b05be329c5f03cf1317aad608ca0a2aac617e397
chourabiseif <chourabiseif94@gmail.com>
2021-04-09 20:21:11 +0100

    first commit

---
+++ src/main/java/com/boutheinawhadil/demo/GitLessonApplication.java
@@ -0,0 +1,15 @@
+package com.boutheinawhadil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLessonApplication.class, args);
+        System.out.println("hello world");
+    }
+
+
+}
---
+++ src/test/java/com/boutheinawhadil/demo/GitLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutheinawhadil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
